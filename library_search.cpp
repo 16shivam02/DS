b0379ce016cad94db4fef45225dc8541674c220b
Shivam <shivamkumar16022@gmail.com>
2026-09-02 16:55:20 +0530

    modified files

--- library_search.cpp
+++ library_search.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 using namespace std;
 
@@ -28,35 +27,4 @@ int main()
 
 cout << "Book not found";
 return 0;
-=======
-#include <iostream>
-using namespace std;
-
-int main()
-{
-    int book[5];
-    int searchID;
-
-    cout << "Enter 5 Book IDs:\n";
-
-    for (int i=0;i<5;i++)
-    {
-        cin >> book[i];
-    }
-
-    cout << "\nEnter Book ID to search:";
-    cin >> searchID;
-
-    for (int i=0;i<5;i++)
-    {
-    if (book[i] == searchID)
-        {
-         cout << "Book found";
-         return 0;
-         }
-    }
-
-cout << "Book not found";
-return 0;
->>>>>>> e6cf0aa1f8ab991eacb17da5350fc36c82276750
 }
